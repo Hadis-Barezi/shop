d36459959e1c09a7be492047883f8fabd6b73d33
Hadis <jebal.barezi.90@gmail.com>
2023-03-03 00:16:53 +0330

    Add translation feature to the fields of Category and Brand class of products' model

--- products/models.py
+++ products/models.py
@@ -12,9 +12,9 @@ class Category(BaseModel):
         verbose_name_plural = _("Categories")
 
     type_choice = [('M', 'Main Category'), ('Sub', 'Sub-Category')]
-    name = models.CharField('Name', max_length=100)
-    type = models.CharField(max_length=3, choices=type_choice)
-    main_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name="sub_cats")
+    name = models.CharField(verbose_name=_('Name'), max_length=100)
+    type = models.CharField(verbose_name=_("Type"), max_length=3, choices=type_choice)
+    main_category = models.ForeignKey('self', verbose_name=_("Main category"), on_delete=models.CASCADE, null=True, blank=True, related_name="sub_cats")
 
     def __str__(self):
         return f"{self.name}"
@@ -26,8 +26,8 @@ class Brand(BaseModel):
         verbose_name = _("Brand")
         verbose_name_plural = _("Brands")
 
-    name = models.CharField(max_length=50, unique=True)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(verbose_name=_("Name"), max_length=50, unique=True)
+    description = models.TextField(verbose_name=_("Description"), null=True, blank=True)
 
 
 
