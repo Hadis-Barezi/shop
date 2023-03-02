19129f3766307bc6622f4b61175483bec9352693
Hadis <jebal.barezi.90@gmail.com>
2023-03-02 23:02:57 +0330

    Add Adress class to accounts' models and register it

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ShopUser
+from .models import ShopUser, Address
 from core.admin import admin_site, MyUserAdmin
 
 
@@ -7,6 +7,19 @@ class ShopUserAdmin(admin.ModelAdmin):
     pass
 
 
+# register ShopUser Model
 admin_site.register(ShopUser, ShopUserAdmin)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'shop_user', 'province', 'city', 'postal_code', 'more_detail')
+    list_filter = ('shop_user', 'province', 'city')
+    search_fields = ('id', 'shop_user', 'postal_code')
+
+
+# register Address Model
+admin_site.register(Address, AddressAdmin)
+
+
 
 
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from core.models import User, BaseModel
 from django.utils.translation import gettext as _
+from django.db import models
 
 
 class ShopUser(User):
@@ -7,3 +8,19 @@ class ShopUser(User):
         verbose_name = _("ShopUser")
         verbose_name_plural = _("ShopUsers")
 
+
+class Address(BaseModel):
+    class Meta:
+        verbose_name = _("Address")
+        verbose_name_plural = _("Addresses")
+
+    province = models.CharField(verbose_name=_("Province"), max_length=50)
+    city = models.CharField(verbose_name=_("City"), max_length=50)
+    street = models.CharField(verbose_name=_("Street"), max_length=50)
+    postal_code = models.CharField(verbose_name=_("Postal code"), max_length=10)
+    more_detail = models.TextField(verbose_name=_("More Detail"), null=True, blank=True)
+    shop_user = models.ForeignKey('ShopUser', verbose_name=_("Shop user"), on_delete=models.CASCADE,
+                                  related_name='addresses')
+
+    def __str__(self):
+        return f"{self.province}-{self.city}-{self.street}-{self.more_detail}- post code: {self.postal_code}"
