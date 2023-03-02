5345f249214a82aad557212259013d212f2acb5c
Hadis <jebal.barezi.90@gmail.com>
2023-03-02 22:38:29 +0330

    Add ShopUser class to accounts' models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from core.models import User, BaseModel
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class ShopUser(User):
+    class Meta:
+        verbose_name = _("ShopUser")
+        verbose_name_plural = _("ShopUsers")
+
