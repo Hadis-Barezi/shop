e17720f96c55b3e52eee5bf61e2e524020f94550
Hadis <jebal.barezi.90@gmail.com>
2023-03-02 23:37:36 +0330

    Fix typo in list_filter CategoryAdmin of accounts' admin

--- products/admin.py
+++ products/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from core.admin import admin_site
-from .models import Category
+from .models import Category, Brand
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'type', 'main_category')
-    list_filter = ('id', 'type', 'type', 'main_category')
+    list_filter = ('id', 'name', 'type', 'main_category')
     search_fields = ('id', 'name', 'type', 'main_category')
 
 
 # register Category
 admin_site.register(Category, CategoryAdmin)
+
+
+
