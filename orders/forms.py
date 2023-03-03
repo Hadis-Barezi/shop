19fc5a91f8b40d313a81d5901a31c0c6d226f441
Hadis <jebal.barezi.90@gmail.com>
2023-03-03 21:42:04 +0330

    Add DiscounTicketForm class and cart view

--- orders/forms.py
+++ orders/forms.py
@@ -3,3 +3,12 @@ from django import forms
 
 class CartItemQuantityForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, widget=forms.NumberInput())
+
+
+class DiscountTicketForm(forms.Form):
+    code = forms.CharField(label="Code", widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+
+    def clean_code(self):
+        if not self.code:
+            return True
+        return False
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from .cart import Cart
+from products.models import Product
+from .forms import CartItemQuantityForm, DiscountTicketForm
 
-# Create your views here.
+
+class ShopCart(View):
+    template_name = 'orders/cart.html'
+    discount_ticket_form = DiscountTicketForm
+
+    def get(self, request):
+        cart = Cart(request)
+        form = self.discount_ticket_form()
+        return render(request, template_name=self.template_name, context={'cart': cart, 'form': form})
+
