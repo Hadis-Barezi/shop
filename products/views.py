29956e56ef4cec37bdbe0b418b0399b1a5828d4a
Hadis <jebal.barezi.90@gmail.com>
2023-03-03 04:10:48 +0330

    Add home view, home template and url

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from . import models
+import random
 
-# Create your views here.
+
+class Home(View):
+    template_name = 'products/home.html'
+    product_model = models.Product
+    num_home_prod = 9
+
+    def get(self, request):
+        products = self.product_model.objects.filter(balance__gt=0)[:self.num_home_prod]
+
+        return render(request, template_name=self.template_name, context={'products': products})
