f289e6d3e6bdda74073079d90a30d4ddcb6ed160
Hadis <jebal.barezi.90@gmail.com>
2023-03-08 17:50:20 +0330

    Ref ShopUserSerializer

--- accounts/api/v1/serializers.py
+++ accounts/api/v1/serializers.py
@@ -3,14 +3,16 @@ from rest_framework import serializers
 
 
 class ShopUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    confirm_password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = models.ShopUser
         fields = ['f_name', 'l_name', 'phone', 'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def validate(self, data):
-        if data ['password'] != data['confirm_password']:
+        if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("Password must be equal!")
         return data
